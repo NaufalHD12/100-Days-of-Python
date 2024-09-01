74ded04abaa80199b961bba050191863867e768e
NaufalHD12 <128771220+NaufalHD12@users.noreply.github.com>
2024-09-01 14:48:37 +0700

    Create 6EscapetheMaze.py
    Link to the maze: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json

---
+++ 6EscapetheMaze.py
@@ -0,0 +1,17 @@
+# Link to the maze: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if wall_on_right():
+        if wall_in_front():
+            turn_left()
+        else:
+            move()
+    else:
+        turn_right()
+        move()
