0433f72a23d28a297aa9b960a135b97a978da372
NaufalHD12 <128771220+NaufalHD12@users.noreply.github.com>
2024-09-09 17:16:42 +0700

    Create 19etch-a-sketch.py

---
+++ 19etch-a-sketch.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def counter_clockwise():
+    tim.left(10)
+
+
+def clockwise():
+    tim.right(10)
+
+
+def clear_drawing():
+    tim.home()
+    tim.clear()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=counter_clockwise)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key="c", fun=clear_drawing)
+screen.exitonclick()
